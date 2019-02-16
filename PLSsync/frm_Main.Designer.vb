7edf98882359feaf87ad9557a53e69fdf127b8d0
Jandalf81 <janschmitte@gmail.com>
2019-02-16 14:20:11 +0100

    renamed Form1 to frm_Main added DataGridView to show connected devices added some resources

---
+++ PLSsync/frm_Main.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_Main
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.grp_Devices = New System.Windows.Forms.GroupBox()
+        Me.dgv_Devices = New System.Windows.Forms.DataGridView()
+        Me.btn_UseSelectedDevice = New System.Windows.Forms.Button()
+        Me.btn_RefreshDevices = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.grp_Devices.SuspendLayout()
+        CType(Me.dgv_Devices, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'grp_Devices
+        '
+        Me.grp_Devices.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.grp_Devices.Controls.Add(Me.btn_RefreshDevices)
+        Me.grp_Devices.Controls.Add(Me.btn_UseSelectedDevice)
+        Me.grp_Devices.Controls.Add(Me.dgv_Devices)
+        Me.grp_Devices.Location = New System.Drawing.Point(12, 12)
+        Me.grp_Devices.Name = "grp_Devices"
+        Me.grp_Devices.Size = New System.Drawing.Size(980, 201)
+        Me.grp_Devices.TabIndex = 1
+        Me.grp_Devices.TabStop = False
+        Me.grp_Devices.Text = "Devices"
+        '
+        'dgv_Devices
+        '
+        Me.dgv_Devices.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dgv_Devices.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv_Devices.Location = New System.Drawing.Point(6, 19)
+        Me.dgv_Devices.Name = "dgv_Devices"
+        Me.dgv_Devices.Size = New System.Drawing.Size(968, 130)
+        Me.dgv_Devices.TabIndex = 0
+        '
+        'btn_UseSelectedDevice
+        '
+        Me.btn_UseSelectedDevice.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_UseSelectedDevice.Location = New System.Drawing.Point(6, 155)
+        Me.btn_UseSelectedDevice.Name = "btn_UseSelectedDevice"
+        Me.btn_UseSelectedDevice.Size = New System.Drawing.Size(924, 38)
+        Me.btn_UseSelectedDevice.TabIndex = 1
+        Me.btn_UseSelectedDevice.Text = "use selected device"
+        Me.btn_UseSelectedDevice.UseVisualStyleBackColor = True
+        '
+        'btn_RefreshDevices
+        '
+        Me.btn_RefreshDevices.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_RefreshDevices.Image = Global.PLSsync.My.Resources.Resources.arrow_refresh
+        Me.btn_RefreshDevices.Location = New System.Drawing.Point(936, 155)
+        Me.btn_RefreshDevices.Name = "btn_RefreshDevices"
+        Me.btn_RefreshDevices.Size = New System.Drawing.Size(38, 38)
+        Me.btn_RefreshDevices.TabIndex = 2
+        Me.btn_RefreshDevices.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 219)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(980, 347)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'frm_Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1004, 578)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.grp_Devices)
+        Me.Name = "frm_Main"
+        Me.Text = "PLSsync"
+        Me.grp_Devices.ResumeLayout(False)
+        CType(Me.dgv_Devices, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents grp_Devices As GroupBox
+    Friend WithEvents dgv_Devices As DataGridView
+    Friend WithEvents btn_RefreshDevices As Button
+    Friend WithEvents btn_UseSelectedDevice As Button
+    Friend WithEvents GroupBox1 As GroupBox
+End Class
---
+++ PLSsync/frm_Main.vb
@@ -0,0 +1,98 @@
+﻿Public Class frm_Main
+    Public deviceList As New List(Of MediaDevices.MediaDevice)
+    Public bs As Equin.ApplicationFramework.BindingListView(Of MediaDevices.MediaDevice) = New Equin.ApplicationFramework.BindingListView(Of MediaDevices.MediaDevice)(deviceList)
+
+
+    Public Sub refreshDevices()
+        Dim devices As IEnumerable(Of MediaDevices.MediaDevice) = MediaDevices.MediaDevice.GetDevices()
+        Dim deviceList As New List(Of MediaDevices.MediaDevice)
+
+        For Each device In devices
+            device.Connect()
+            deviceList.Add(device)
+        Next
+
+        'bs = New Equin.ApplicationFramework.BindingListView(Of MediaDevices.MediaDevice)(deviceList)
+        dgv_Devices.DataSource = deviceList
+    End Sub
+
+
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        Dim devices As IEnumerable(Of MediaDevices.MediaDevice) = MediaDevices.MediaDevice.GetDevices()
+
+        For Each device In devices
+            MsgBox("FriendlyName: " + device.FriendlyName + vbCrLf +
+                   "Description: " + device.Description + vbCrLf +
+                   "Manufacturer: " + device.Manufacturer)
+
+            If (device.Description = "Pixel 2") Then
+                device.Connect()
+
+                Dim directories As IEnumerable(Of String) = device.EnumerateDirectories("/", "", IO.SearchOption.TopDirectoryOnly)
+                For Each directory In directories
+                    MsgBox("Directory: " + directory)
+                Next
+
+                Dim contentLocations As IEnumerable(Of String) = device.GetContentLocations(MediaDevices.ContentType.Audio)
+                For Each contentLocation In contentLocations
+                    MsgBox("Content Location: " + contentLocation)
+                Next
+            End If
+        Next
+    End Sub
+
+    Private Sub frm_Main_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ' settings for DataGridView
+        With dgv_Devices
+            .ReadOnly() = True
+            .AllowUserToAddRows = False
+            .AutoGenerateColumns = False
+            .SelectionMode = DataGridViewSelectionMode.FullRowSelect
+            .AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells
+        End With
+
+        ' prepare columns for DataGridView
+        Dim col_FriendlyName As New DataGridViewTextBoxColumn()
+        col_FriendlyName.DataPropertyName = "FriendlyName"
+        col_FriendlyName.Name = "Friendly Name"
+
+        Dim col_Description As New DataGridViewTextBoxColumn()
+        col_Description.DataPropertyName = "Description"
+        col_Description.Name = "Description"
+
+        Dim col_Manufacturer As New DataGridViewTextBoxColumn()
+        col_Manufacturer.DataPropertyName = "Manufacturer"
+        col_Manufacturer.Name = "Manufacturer"
+
+        Dim col_DeviceType As New DataGridViewTextBoxColumn()
+        col_DeviceType.DataPropertyName = "DeviceType"
+        col_DeviceType.Name = "DeviceType"
+
+        Dim col_PowerLevel As New DataGridViewTextBoxColumn()
+        col_PowerLevel.DataPropertyName = "PowerLevel"
+        col_PowerLevel.Name = "PowerLevel"
+
+        Dim col_SerialNumber As New DataGridViewTextBoxColumn()
+        col_SerialNumber.DataPropertyName = "SerialNumber"
+        col_SerialNumber.Name = "SerialNumber"
+
+        ' add prepared columns to DataGridView
+        With dgv_Devices.Columns
+            .Add(col_Manufacturer)
+            .Add(col_Description)
+            .Add(col_DeviceType)
+            .Add(col_FriendlyName)
+            .Add(col_PowerLevel)
+            .Add(col_SerialNumber)
+        End With
+
+        refreshDevices()
+    End Sub
+
+    Private Sub btn_RefreshDevices_Click(sender As Object, e As EventArgs) Handles btn_RefreshDevices.Click
+        refreshDevices()
+    End Sub
+End Class
