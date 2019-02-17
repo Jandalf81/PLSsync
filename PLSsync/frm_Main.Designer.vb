8ecd1f4f8c8aff2f3e403e5fa111951fe8c8d22e
Jandalf81 <janschmitte@gmail.com>
2019-02-17 14:42:07 +0100

    changed DataSource of DataGridView to devices added function to get selected Device from DataGridView added control element to show storage information of the selected Device

--- PLSsync/frm_Main.Designer.vb
+++ PLSsync/frm_Main.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class frm_Main
     Inherits System.Windows.Forms.Form
 
     'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,15 +20,23 @@ Partial Class frm_Main
     'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
     'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.grp_Devices = New System.Windows.Forms.GroupBox()
-        Me.dgv_Devices = New System.Windows.Forms.DataGridView()
-        Me.btn_UseSelectedDevice = New System.Windows.Forms.Button()
         Me.btn_RefreshDevices = New System.Windows.Forms.Button()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btn_UseSelectedDevice = New System.Windows.Forms.Button()
+        Me.dgv_Devices = New System.Windows.Forms.DataGridView()
+        Me.grp_Sync = New System.Windows.Forms.GroupBox()
+        Me.grp_SelectedDevice = New System.Windows.Forms.GroupBox()
+        Me.pic_Progress = New System.Windows.Forms.PictureBox()
+        Me.lbl_Sync_StorageInfo = New System.Windows.Forms.Label()
+        Me.txt_Sync_DeviceName = New System.Windows.Forms.TextBox()
+        Me.lbl_Sync_DeviceName = New System.Windows.Forms.Label()
         Me.grp_Devices.SuspendLayout()
         CType(Me.dgv_Devices, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.grp_Sync.SuspendLayout()
+        Me.grp_SelectedDevice.SuspendLayout()
+        CType(Me.pic_Progress, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'grp_Devices
@@ -45,15 +53,15 @@ Partial Class frm_Main
         Me.grp_Devices.TabStop = False
         Me.grp_Devices.Text = "Devices"
         '
-        'dgv_Devices
+        'btn_RefreshDevices
         '
-        Me.dgv_Devices.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.dgv_Devices.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgv_Devices.Location = New System.Drawing.Point(6, 19)
-        Me.dgv_Devices.Name = "dgv_Devices"
-        Me.dgv_Devices.Size = New System.Drawing.Size(968, 130)
-        Me.dgv_Devices.TabIndex = 0
+        Me.btn_RefreshDevices.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn_RefreshDevices.Image = Global.PLSsync.My.Resources.Resources.arrow_refresh
+        Me.btn_RefreshDevices.Location = New System.Drawing.Point(936, 155)
+        Me.btn_RefreshDevices.Name = "btn_RefreshDevices"
+        Me.btn_RefreshDevices.Size = New System.Drawing.Size(38, 38)
+        Me.btn_RefreshDevices.TabIndex = 2
+        Me.btn_RefreshDevices.UseVisualStyleBackColor = True
         '
         'btn_UseSelectedDevice
         '
@@ -66,39 +74,97 @@ Partial Class frm_Main
         Me.btn_UseSelectedDevice.Text = "use selected device"
         Me.btn_UseSelectedDevice.UseVisualStyleBackColor = True
         '
-        'btn_RefreshDevices
+        'dgv_Devices
         '
-        Me.btn_RefreshDevices.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.btn_RefreshDevices.Image = Global.PLSsync.My.Resources.Resources.arrow_refresh
-        Me.btn_RefreshDevices.Location = New System.Drawing.Point(936, 155)
-        Me.btn_RefreshDevices.Name = "btn_RefreshDevices"
-        Me.btn_RefreshDevices.Size = New System.Drawing.Size(38, 38)
-        Me.btn_RefreshDevices.TabIndex = 2
-        Me.btn_RefreshDevices.UseVisualStyleBackColor = True
+        Me.dgv_Devices.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dgv_Devices.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv_Devices.Location = New System.Drawing.Point(6, 19)
+        Me.dgv_Devices.Name = "dgv_Devices"
+        Me.dgv_Devices.Size = New System.Drawing.Size(968, 130)
+        Me.dgv_Devices.TabIndex = 0
         '
-        'GroupBox1
+        'grp_Sync
         '
-        Me.GroupBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+        Me.grp_Sync.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.GroupBox1.Location = New System.Drawing.Point(12, 219)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(980, 347)
-        Me.GroupBox1.TabIndex = 2
-        Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "GroupBox1"
+        Me.grp_Sync.Controls.Add(Me.grp_SelectedDevice)
+        Me.grp_Sync.Location = New System.Drawing.Point(12, 219)
+        Me.grp_Sync.Name = "grp_Sync"
+        Me.grp_Sync.Size = New System.Drawing.Size(980, 347)
+        Me.grp_Sync.TabIndex = 2
+        Me.grp_Sync.TabStop = False
+        Me.grp_Sync.Text = "Sync"
+        '
+        'grp_SelectedDevice
+        '
+        Me.grp_SelectedDevice.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.grp_SelectedDevice.Controls.Add(Me.pic_Progress)
+        Me.grp_SelectedDevice.Controls.Add(Me.lbl_Sync_StorageInfo)
+        Me.grp_SelectedDevice.Controls.Add(Me.txt_Sync_DeviceName)
+        Me.grp_SelectedDevice.Controls.Add(Me.lbl_Sync_DeviceName)
+        Me.grp_SelectedDevice.Location = New System.Drawing.Point(6, 19)
+        Me.grp_SelectedDevice.Name = "grp_SelectedDevice"
+        Me.grp_SelectedDevice.Size = New System.Drawing.Size(461, 138)
+        Me.grp_SelectedDevice.TabIndex = 0
+        Me.grp_SelectedDevice.TabStop = False
+        Me.grp_SelectedDevice.Text = "selected Device"
+        '
+        'pic_Progress
+        '
+        Me.pic_Progress.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pic_Progress.Location = New System.Drawing.Point(111, 43)
+        Me.pic_Progress.Name = "pic_Progress"
+        Me.pic_Progress.Size = New System.Drawing.Size(344, 20)
+        Me.pic_Progress.TabIndex = 5
+        Me.pic_Progress.TabStop = False
+        '
+        'lbl_Sync_StorageInfo
+        '
+        Me.lbl_Sync_StorageInfo.AutoSize = True
+        Me.lbl_Sync_StorageInfo.Location = New System.Drawing.Point(7, 47)
+        Me.lbl_Sync_StorageInfo.Name = "lbl_Sync_StorageInfo"
+        Me.lbl_Sync_StorageInfo.Size = New System.Drawing.Size(68, 13)
+        Me.lbl_Sync_StorageInfo.TabIndex = 2
+        Me.lbl_Sync_StorageInfo.Text = "Storage Info:"
+        '
+        'txt_Sync_DeviceName
+        '
+        Me.txt_Sync_DeviceName.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txt_Sync_DeviceName.Location = New System.Drawing.Point(111, 17)
+        Me.txt_Sync_DeviceName.Name = "txt_Sync_DeviceName"
+        Me.txt_Sync_DeviceName.ReadOnly = True
+        Me.txt_Sync_DeviceName.Size = New System.Drawing.Size(344, 20)
+        Me.txt_Sync_DeviceName.TabIndex = 1
+        '
+        'lbl_Sync_DeviceName
+        '
+        Me.lbl_Sync_DeviceName.AutoSize = True
+        Me.lbl_Sync_DeviceName.Location = New System.Drawing.Point(7, 20)
+        Me.lbl_Sync_DeviceName.Name = "lbl_Sync_DeviceName"
+        Me.lbl_Sync_DeviceName.Size = New System.Drawing.Size(75, 13)
+        Me.lbl_Sync_DeviceName.TabIndex = 0
+        Me.lbl_Sync_DeviceName.Text = "Device Name:"
         '
         'frm_Main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1004, 578)
-        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.grp_Sync)
         Me.Controls.Add(Me.grp_Devices)
         Me.Name = "frm_Main"
         Me.Text = "PLSsync"
         Me.grp_Devices.ResumeLayout(False)
         CType(Me.dgv_Devices, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.grp_Sync.ResumeLayout(False)
+        Me.grp_SelectedDevice.ResumeLayout(False)
+        Me.grp_SelectedDevice.PerformLayout()
+        CType(Me.pic_Progress, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -106,5 +172,10 @@ Partial Class frm_Main
     Friend WithEvents dgv_Devices As DataGridView
     Friend WithEvents btn_RefreshDevices As Button
     Friend WithEvents btn_UseSelectedDevice As Button
-    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents grp_Sync As GroupBox
+    Friend WithEvents grp_SelectedDevice As GroupBox
+    Friend WithEvents lbl_Sync_DeviceName As Label
+    Friend WithEvents txt_Sync_DeviceName As TextBox
+    Friend WithEvents lbl_Sync_StorageInfo As Label
+    Friend WithEvents pic_Progress As PictureBox
 End Class
--- PLSsync/frm_Main.vb
+++ PLSsync/frm_Main.vb
@@ -1,19 +1,21 @@
 ﻿Public Class frm_Main
-    Public deviceList As New List(Of MediaDevices.MediaDevice)
-    Public bs As Equin.ApplicationFramework.BindingListView(Of MediaDevices.MediaDevice) = New Equin.ApplicationFramework.BindingListView(Of MediaDevices.MediaDevice)(deviceList)
+    Public devices As IEnumerable(Of MediaDevices.MediaDevice)
+    Public selectedDevice As MediaDevices.MediaDevice
+    'Public deviceList As New List(Of MediaDevices.MediaDevice)
+    'Public bs As Equin.ApplicationFramework.BindingListView(Of MediaDevices.MediaDevice) = New Equin.ApplicationFramework.BindingListView(Of MediaDevices.MediaDevice)(deviceList)
 
 
     Public Sub refreshDevices()
-        Dim devices As IEnumerable(Of MediaDevices.MediaDevice) = MediaDevices.MediaDevice.GetDevices()
-        Dim deviceList As New List(Of MediaDevices.MediaDevice)
+        devices = MediaDevices.MediaDevice.GetDevices()
+        'Dim deviceList As New List(Of MediaDevices.MediaDevice)
 
         For Each device In devices
             device.Connect()
-            deviceList.Add(device)
+            ' deviceList.Add(device)
         Next
 
         'bs = New Equin.ApplicationFramework.BindingListView(Of MediaDevices.MediaDevice)(deviceList)
-        dgv_Devices.DataSource = deviceList
+        dgv_Devices.DataSource = devices
     End Sub
 
 
@@ -94,5 +96,60 @@
 
     Private Sub btn_RefreshDevices_Click(sender As Object, e As EventArgs) Handles btn_RefreshDevices.Click
         refreshDevices()
+    End Sub
+
+    Private Sub btn_UseSelectedDevice_Click(sender As Object, e As EventArgs) Handles btn_UseSelectedDevice.Click
+        ' get currently selected device from DataGridView
+        selectedDevice = dgv_Devices.SelectedRows(0).DataBoundItem
+
+        txt_Sync_DeviceName.Text = String.Format("{0}, {1}, {2}", selectedDevice.Manufacturer, selectedDevice.Description, selectedDevice.SerialNumber)
+
+        pic_Progress.Refresh()
+    End Sub
+
+
+
+    Private Sub pic_Progress_Paint(sender As Object, e As PaintEventArgs) Handles pic_Progress.Paint
+        If selectedDevice Is Nothing Then
+            e.Graphics.DrawRectangle(New Pen(Brushes.Gray, 1), 0, 0, pic_Progress.ClientSize.Width - 1, pic_Progress.ClientSize.Height - 1)
+        Else
+            Dim objects = selectedDevice.FunctionalObjects(MediaDevices.FunctionalCategory.Storage)
+            Dim storageInfo As MediaDevices.MediaStorageInfo = selectedDevice.GetStorageInfo(objects.First())
+
+            Dim percent As Integer = Math.Round(storageInfo.FreeSpaceInBytes * 100 / storageInfo.Capacity, 0)
+            Dim color As Brush
+
+            Select Case percent
+                Case < 10
+                    color = Brushes.Red
+                Case < 20
+                    color = Brushes.Orange
+                Case Else
+                    color = Brushes.LightGreen
+            End Select
+
+            ' reset
+            e.Graphics.Clear(pic_Progress.BackColor)
+
+            ' draw the background and border
+            Dim fraction As Single = storageInfo.FreeSpaceInBytes / storageInfo.Capacity
+            Dim width As Integer = fraction * pic_Progress.ClientSize.Width
+
+            e.Graphics.FillRectangle(color, 0, 0, width, pic_Progress.ClientSize.Height)
+            e.Graphics.DrawRectangle(New Pen(Brushes.Gray, 1), 0, 0, pic_Progress.ClientSize.Width - 1, pic_Progress.ClientSize.Height - 1)
+
+            ' draw the text
+            e.Graphics.TextRenderingHint = Drawing.Text.TextRenderingHint.AntiAliasGridFit
+
+            Dim sf As New StringFormat()
+            sf.Alignment = StringAlignment.Near
+            sf.LineAlignment = StringAlignment.Center
+
+            e.Graphics.DrawString(String.Format("{0} GB free of {1} GB ({2}%)", Math.Round(storageInfo.FreeSpaceInBytes / 1024 / 1024 / 1024, 2), Math.Round(storageInfo.Capacity / 1024 / 1024 / 1024, 2), percent), Me.Font, Brushes.Black, pic_Progress.ClientRectangle, sf)
+        End If
+    End Sub
+
+    Private Sub frm_Main_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        pic_Progress.Refresh()
     End Sub
 End Class
